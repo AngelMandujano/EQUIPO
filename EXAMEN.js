48f97177cdd0f0923c47aabe3b8d3a892d32e3ea
AngelMandujano <89949647+AngelMandujano@users.noreply.github.com>
2021-10-06 14:19:43 -0500

    examen

---
+++ EXAMEN.js
@@ -0,0 +1,289 @@
+var seleccion= prompt("ingresa el numero del equipo que quieres ver ");
+
+var equipo1 ={
+    "diego alarcon":{
+        "diego alarcon":{
+            "numero de registro":1321124112,
+            "edad":25,
+            "sexo":"masculino",
+            "telefono":5515157802,
+            "5 habilidades posiciones": "velocidad,resistencia,liderazgo,compromiso,delantero",
+            "5 equipos donde ha jugado": "chivas, america,toluca,pachuca,atlas"
+
+
+        
+        },
+    },
+    "jose angel":{
+        "numero de registro":1321124145,
+        "edad":26,
+        "sexo":"masculino",
+        "telefono":5515157902,
+        "5 habilidades posiciones": "velocidad,contra golpe ,liderazgo,compromiso,portero ",
+        "5 equipos donde ha jugado": "chivas, cruz azul,toluca,zacatepec,atlas"
+
+    },
+    "gael barrera":{
+        "":{
+            "numero de registro":1312124145,
+            "edad":21,
+            "sexo":"masculino",
+            "telefono":7715157902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,defensa lateral derecha",
+            "5 equipos donde ha jugado": "chivas, cruz azul,toluca,puebla,atlas"
+    
+    },
+    },
+    "fredy lombard":{
+        "":{
+            "numero de registro":1512124145,
+            "edad":27,
+            "sexo":"masculino",
+            "telefono":7785157902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,medio campo",
+            "5 equipos donde ha jugado": "chivas, cruz azul,toluca,puebla,atlas"
+    
+    },
+    },
+    "rigo tarantino":{
+        "":{
+            "numero de registro":1512124145,
+            "edad":27,
+            "sexo":"masculino",
+            "telefono":7785157902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,medio campo",
+            "5 equipos donde ha jugado": "chivas, cruz azul,toluca,puebla,atlas"
+    
+    },
+    },
+}
+//switch(seleccion){
+    //case "elemento1":
+       // console.log(elemento1);
+       // break;
+
+var equipo2 ={
+    "alejandra macias":{
+        "alejandra macias ":{
+            "numero de registro":13496124112,
+            "edad":20,
+            "sexo":"femenino",
+            "telefono":5515157802,
+            "5 habilidades posiciones": "velocidad,resistencia,liderazgo,compromiso,delantero",
+            "5 equipos donde ha jugado": "juventus , america,toluca,pachuca,atlas"
+
+
+        
+        },
+    },
+    "karen munjica":{
+        "numero de registro":134941124145,
+        "edad":19,
+        "sexo":"femenino",
+        "telefono":5515207902,
+        "5 habilidades posiciones": "velocidad,contra golpe ,liderazgo,compromiso,medio campo",
+        "5 equipos donde ha jugado": "chivas, argentin,toluca,zacatepec,atlas"
+
+    },
+    "mario bautuista":{
+        "":{
+            "numero de registro":1312104145,
+            "edad":21,
+            "sexo":"masculino",
+            "telefono":7711557902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,medio campo",
+            "5 equipos donde ha jugado": "chivas,españa,toluca,puebla,atlas"
+    
+    },
+    },
+    "daniel rodrigez":{
+        "":{
+            "numero de registro":11692124145,
+            "edad":27,
+            "sexo":"masculino",
+            "telefono":7796157902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,medio campo",
+            "5 equipos donde ha jugado": "chivas, cruz azul,toluca,colima,atlas"
+    
+    },
+    },
+    "pedrino pazcal":{
+        "":{
+            "numero de registro":1512024145,
+            "edad":35,
+            "sexo":"masculino",
+            "telefono":7720157902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,medio campo",
+            "5 equipos donde ha jugado": "brazil, cruz azul,toluca,puebla,atlas"
+    
+    },
+    },
+}
+var equipo3={
+    "silvia pedraza":{
+        "silvia pedraza":{
+            "numero de registro":1321122012,
+            "edad":26,
+            "sexo":"femenino",
+            "telefono":5515107802,
+            "5 habilidades posiciones": "velocidad,resistencia,liderazgo,compromiso,delantero",
+            "5 equipos donde ha jugado": "chivas, america,toluca,pachuca,barcelona"
+
+
+        
+        },
+    },
+    "pedro acuña":{
+        "numero de registro":13211241164,
+        "edad":28,
+        "sexo":"masculino",
+        "telefono":5516157902,
+        "5 habilidades posiciones": "velocidad,contra golpe ,liderazgo,compromiso,medio campo",
+        "5 equipos donde ha jugado": "chivas, cruz azul,toluca,santo domingo,atlas"
+
+    },
+    "julia zuniga":{
+        "":{
+            "numero de registro":1512124145,
+            "edad":29,
+            "sexo":"femenino",
+            "telefono":7715197902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,delantero ",
+            "5 equipos donde ha jugado": "chivas, cruz azul,toluca,puebla,atlas"
+    
+    },
+    },
+    "perla lopez":{
+        "":{
+            "numero de registro":1513024145,
+            "edad":27,
+            "sexo":"femenino",
+            "telefono":5685157902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,defensa central",
+            "5 equipos donde ha jugado": "los cuervos de nuevo toledo, cruz azul,barcelona,puebla,atlas"
+    
+    },
+    },
+    "alexis vazques ":{
+        "":{
+            "numero de registro":1812124145,
+            "edad":27,
+            "sexo":"masculino",
+            "telefono":77565157902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,defensa lateral izquierdo",
+            "5 equipos donde ha jugado": "milan, cruz azul,toluca,puebla,atlas"
+    
+    },
+    },
+}
+var equipo4 ={
+    "karla rivera":{
+        "karla rivera":{
+            "numero de registro":2521124112,
+            "edad":26,
+            "sexo":"femenino",
+            "telefono":5516157802,
+            "5 habilidades posiciones": "velocidad,resistencia,liderazgo,compromiso,defensa lateral derecho",
+            "5 equipos donde ha jugado": "chivas, america,toluca,pachuca,atlas"
+
+
+        
+        },
+    },
+    "paola peralta":{
+        "numero de registro":8921124145,
+        "edad":38,
+        "sexo":"femenino",
+        "telefono":55156017902,
+        "5 habilidades posiciones": "velocidad,contra golpe ,liderazgo,compromiso,lateral izquierdo",
+        "5 equipos donde ha jugado": "chivas, cruz azul,toluca,zacatepec,atlas"
+
+    },
+    "zuri pastrana":{
+        "":{
+            "numero de registro":60112124145,
+            "edad":29,
+            "sexo":"femenino",
+            "telefono":7720157902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,portero",
+            "5 equipos donde ha jugado": "chivas, cruz azul,toluca,puebla,atlas"
+    
+    },
+    },
+    "federica romano":{
+        "":{
+            "numero de registro":1512119145,
+            "edad":27,
+            "sexo":"femenino",
+            "telefono":7736157902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,defensa central",
+            "5 equipos donde ha jugado": "chivas, cruz azul,toluca,puebla,atlas"
+    
+    },
+    },
+    "maria castillo":{
+        "":{
+            "numero de registro":9012124145,
+            "edad":28,
+            "sexo":"femenino",
+            "telefono":10285157902,
+            "5 habilidades posiciones": "velocidad,contra golpe ,precion a contra ataque,compromiso,defensa",
+            "5 equipos donde ha jugado": "chivas, cruz azul,toluca,puebla,atlas"
+    
+    },
+    },
+}
+var equipo5 ={
+    "jovana chaires":{
+        "jovana chaires":{
+            "numero de registro":5621124112,
+            "edad":18,
+            "sexo":"femenino",
+            "telefono":5515180802,
+            "5 habilidades posiciones": "velocidad,resistencia,liderazgo,compromiso,delantero",
+            "5 equipos donde ha jugado": "chivas, america,toluca,pachuca,atlas"
+
+
+        
+        },
+    },
+    "alexa barrios":{
+        "numero de registro":1321124160,
+        "edad":32,
+        "sexo":"femenino",
+        "telefono":5529157902,
+        "5 habilidades posiciones": "velocidad,contra golpe ,liderazgo,compromiso,delantero",
+        "5 equipos donde ha jugado": "chivas, cruz azul,toluca,zacatepec,atlas"
+
+    },
+    
+    
+    "lana rhoades":{
+        "":{
+            "numero de registro":2012124145,
+            "edad":28,
+            "sexo":"femenino",
+            "telefono":7787157902,
+            "5 habilidades posiciones": ["velocidad","contra golpe","precion a contra ataque","compromiso,portera"],
+            "5 equipos donde ha jugado": "chivas, cruz azul,toluca,puebla,atlas"
+    
+    },
+    },
+}
+switch(seleccion){
+    case "equipo1":
+        console.log(equipo1);
+        break;
+        case "equipo2":
+        console.log(equipo2);
+        break;
+        case "equipo3":
+        console.log(equipo3);
+        break;
+        case "equipo4":
+        console.log(equipo4);
+        break;
+        case "equipo5":
+        console.log(equipo5);
+        break;
+}
